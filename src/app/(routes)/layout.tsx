5bc5713ab53f288077efc96f0f600198ccbfb7a9
Ziad Mahmoud <ziaddmahmoude@gmail.com>
2025-04-24 23:53:27 +0200

    feat: update product name and change favicon

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -3,6 +3,9 @@ import Providers from "@/components/providers";
 export default function LocaleLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
+      <head>
+        <title>Elevate</title>
+      </head>
       <body>
         <Providers>
           {/* Main */}
