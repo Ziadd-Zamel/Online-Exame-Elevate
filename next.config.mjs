8b3b65f4029c319dac61d73570d049e444ec2768
Ziad Mahmoud <ziaddmahmoude@gmail.com>
2025-04-24 22:58:48 +0200

    fix(config): update Next.js config to include image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["exam.elevateegy.com"],
+  },
+};
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
