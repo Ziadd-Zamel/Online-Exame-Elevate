e21495b4f9b2436bcd14d9184dea70101f22f6c8
Ziad Mahmoud <ziaddmahmoude@gmail.com>
2025-04-24 22:51:28 +0200

    feat(auth): add authentication layout

---
+++ src/app/(routes)/auth/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AuthLayout from "./_components/auth-layout";
+
+const layout = ({ children }: LayoutProps) => {
+  return <AuthLayout>{children}</AuthLayout>;
+};
+
+export default layout;
