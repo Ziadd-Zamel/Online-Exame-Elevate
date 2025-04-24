31bd2fe62880a4609b9fdbcec1e374bff8552533
Ziad Mahmoud <ziaddmahmoude@gmail.com>
2025-04-24 22:29:50 +0200

    refactor(i18n): remove all next-intl integration and related code

--- src/app/[locale]/(homepage)/page.tsx
+++
@@ -1,8 +0,0 @@
-import { useTranslations } from "next-intl";
-
-export default function Home() {
-  // Translation
-  const t = useTranslations();
-
-  return <main className="min-h-screen flex items-center justify-center font-semibold text-5xl">{t("hello")}</main>;
-}
--- src/app/[locale]/auth/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AuthLayout from './_components/auth-layout';
-
-const layout = ({ children }: LayoutProps) => {
-  return <AuthLayout>{children}</AuthLayout>;
-};
-
-export default layout;
